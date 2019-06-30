166f41a90a6feeae8213276c5c0bba179d0b18d2
Nitesh <tiwari.nitesh294@gmail.com>
2019-06-30 18:05:24 +0530

    form changes

--- src/app/book-event/book-event.component.ts
+++ src/app/book-event/book-event.component.ts
@@ -15,8 +15,8 @@ export class BookEventComponent implements OnInit {
   email:string;
   phone:any;
   seatCount:number=2;
-  attendees:string[];
-  model: any = {};
+  aFlag:boolean=false;
+  attendees:any=[];
 
   constructor(private r: ActivatedRoute,private route:Router) { 
     r.url.subscribe((u) => {
@@ -29,8 +29,16 @@ export class BookEventComponent implements OnInit {
 
   }
 
-  onSubmit() {
-    console.log()
+  myFunc(count){
+    this.attendees = [];
+    for(let i=0;i<count;i++){
+      this.attendees.push('');
+    }
+    this.aFlag = true;
+  }
+
+  onSubmit(f) {
+    console.log(f,this.userName,this.email,this.phone,this.seatCount,this.attendees)
   }
   redirect(){
     this.route.navigate(['']);
